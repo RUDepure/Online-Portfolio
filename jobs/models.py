fdef334cc818d5ef8ecbbd75cc2a3cdddb02f35d
RUDepure <alexrum92@gmail.com>
2023-02-28 23:57:23 -0600

    Created the Job class for our models with properties of Image and Summary. Installed Pillow.

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+#https://www.linkedin.com/learning/building-a-personal-portfolio-with-django/creating-the-models-in-django?autoSkip=true&autoplay=true&resume=false
 
 # Create your models here.
+# A model is a python class that can be saved into a database
+
+class Job(models.Model):
+    # Images
+    # New property, that is a type of ImageField
+    # ImageField is data that is going to be saved as an Image
+    # Saved in a folder named "images"
+    # NEED TO pip install PILLOW FOR IMAGES TO WORK
+    image = models.ImageField(upload_to = 'images/')
+    
+    # Summary
+    # New property that is type CharField
+    # CharField that is data saved as text
+    # It has a maximum number of characters of 200
+    summary = models.CharField(max_length = 200)
