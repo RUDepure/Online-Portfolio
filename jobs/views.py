46f7fbdb57aee8f1d63002205c72f57b82b7745f
RUDepure <alexrum92@gmail.com>
2023-03-06 19:13:36 -0600

    Implemented the basic functionality that lets Django display in our HTML file the values stored in our Database

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+# Import our Job model to be able to display it in our site
+from .models import Job
 
 # Create your views here.
 #Django needs a Templates Folder in which we store our html files
 def home(request):
+    jobs = Job.objects
     #renders the html file inside templates/jobs folder
-    return render(request, 'jobs/home.html')
+    return render(request, 'jobs/home.html', {'jobs': jobs}) #this render passes the Job model's objects as a list to home.html
